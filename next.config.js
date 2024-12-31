f4aa385b2cb222dfa714dccf0365a08622c3d4f4
alex_Palladis <alehandro4443@gmail.com>
2024-12-31 21:13:18 +0200

    heroku deployement

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
+  reactStrictMode: true,
   typescript:{
     ignoreBuildErrors:true
-  }
-  
+  },
+  experimental: {
+    outputFileTracing: true
+  },
 };
 
 module.exports = nextConfig;
