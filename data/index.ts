4dd72bf427f6b4ed5bb061b71f5976aed19e3f72
alex_Palladis <alehandro4443@gmail.com>
2024-12-31 21:04:59 +0200

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,136 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Turning ideas into impact with collaboration and clear purpose",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I am flexible with time zones and can adjust my hours as needed.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve...",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for software development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Food Order Application",
+      description: "FoodieApp",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Would you like to contact me?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "BankBuddy App",
+      des: "A modern banking web application to manage accounts, view transaction history, transfer funds, and connect to financial services securely and efficiently.",
+      img: "/banking.svg",
+      iconLists: ["/re.svg", "next.svg","/tail.svg", "/ts.svg",'/appwrite.svg'],
+      link: "https://banking-app-01c9d8999292.herokuapp.com/",
+    },
+    {
+      id: 2,
+      title: "DreamHome App",
+      des: "DreamHome App is an innovative platform designed for users to showcase their dream homes, connect with landlords, and manage their profiles with ease. It offers a seamless experience for house hunting and communication within a user-friendly interface.",
+      img: "/dream.svg",
+      iconLists: ["/re.svg","/node.svg", "/tail.svg", "/ts.svg", "/mongo.svg"],
+      link: "https://main.d2v5tsefsm99cr.amplifyapp.com/",
+    },
+    {
+      id: 3,
+      title: "FavPlaces App",
+      des: "FavPlaces is a full-stack web application where users can create an account, upload images of their favorite places around the world, and view them on an interactive Google Maps interface. The app is built using React for the frontend, Node.js and Express for the backend, and MongoDB with Mongoose for the database.",
+      img: "/fav.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/node.svg", "/javascript.svg","/mongo.svg"],
+      link: "https://main.d3kwcjrbt2gy3t.amplifyapp.com/",
+    },
+    {
+      id: 4,
+      title: "Workout Manager App",
+      des: "A full-stack MERN application to help users add, manage, and track their workouts with user authentication and secure data handling.",
+      img: "/workout.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/node.svg", "/javascript.svg", "/sql.svg"],
+      link: "https://main.dmh0b2wygl91q.amplifyapp.com/login",
+    },
+  ];
+  
+ 
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Full Stack Developer",
+      desc: "I have been working as a freelance developer for the past year, independently handling and delivering various projects. This ongoing experience has helped me enhance my skills in full-stack development and problem-solving. I continue to adapt, learn new technologies, and deliver effective solutions.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Former Military Officer",
+      desc: "I previously served as a military officer for 11 years, where I developed leadership, discipline, and teamwork skills. My role required strategic thinking, decision-making under pressure, and adaptability. This experience shaped my ability to excel in high-stakes environments.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:'https://github.com/alexPalladis'
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+      link:'https://www.linkedin.com/in/alexandros-palladis/'
+    },
+  ];
