2dc2f8366dd2bb3a62578e5adacd76b4ff07142d
alex_Palladis <alehandro4443@gmail.com>
2025-01-07 20:18:28 +0200

    finalize deployment

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Solutions with Next.js
+            Welcome to my dynamic Portfolio...
           </p>
 
           <TextGenerateEffect
